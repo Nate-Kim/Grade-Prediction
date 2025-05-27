2e72ca2e06680ac6010821dd004d1d7ae17d8957
Michael <12264379+akhotam@users.noreply.github.com>
2025-05-26 23:21:24 -0700

    Added logo, project name to header

--- web_ui/frontend/src/components/Header.jsx
+++ web_ui/frontend/src/components/Header.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../assets/grade_predictor.png'
 
 export default function Header() {
   return (
     <header className='header'>
+      <div className="logo-section">
+        <img src={logo} alt="Logo" className='logo-img' />
+        <span className="logo-text">Grade Predictor</span>
+      </div>
+      <nav className="nav-buttons">
       <Link className="nav-button" to="/">HOME</Link>
       <Link className="nav-button" to="/about">ABOUT</Link>
+      </nav>
     </header>
   );
 }
