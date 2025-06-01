7bbdd38693c333a223b707c973821d56f38a0acc
Michael <12264379+akhotam@users.noreply.github.com>
2025-05-31 18:59:33 -0700

    Changed home page containers so param and predict are side by side

--- web_ui/frontend/src/components/Tooltip.jsx
+++ web_ui/frontend/src/components/Tooltip.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Tooltip.css';
 
-const Tooltip = ({ text, tooltipText }) => {
+const Tooltip = ({ tooltipText }) => {
   return (
     <div className="info-tooltip">
       <span className="info-icon">i</span>
